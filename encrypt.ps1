a6502d0f3e89fa44579f45c67fce15eede701226
Aron Kornbluh <104830414+AronKorn@users.noreply.github.com>
2022-05-03 13:42:23 +0300

    Add files via upload

---
+++ encrypt.ps1
@@ -0,0 +1,7 @@
+﻿# Selects all the files in the folder
+ForEach ($FileToEncrypt in Get-ChildItem "C:\Users\administrator\Desktop\pass") {
+# Encrypts all files with the public key, and adds the ".encrypted" extension to the name of the encrypted files
+openssl pkeyutl -encrypt -pubin -inkey publickey.crt -in $FileToEncrypt.FullName -out ($FileToEncrypt.FullName + ".encrypted")
+}
+# Deletes all unencrypted files in the folder
+Remove-Item -recurse C:\Users\administrator\Desktop\pass\* -exclude *.encrypted
