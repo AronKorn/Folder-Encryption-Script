d4f9090c4b3978a4fd66abe92e0f6b1b18109b95
Aron Kornbluh <104830414+AronKorn@users.noreply.github.com>
2022-05-17 15:03:11 +0300

    Update encrypt.ps1

--- encrypt.ps1
+++ encrypt.ps1
@@ -1,7 +1,15 @@
-﻿# Selects all the files in the folder
-ForEach ($FileToEncrypt in Get-ChildItem "C:\Users\administrator\Desktop\pass") {
+# Choose which folder to encrypt
+$FolderPath = Read-Host "Please enter the path to the folder you want to encrypt their files"
+
+# Select the public key you want to use to encrypt
+$PubKey = Read-Host "Please enter the path to the Public Key you want to use to encrypt the files"
+
+# Selects all the files in the folder
+ForEach ($FileToEncrypt in Get-ChildItem $FolderPath) {
+
 # Encrypts all files with the public key, and adds the ".encrypted" extension to the name of the encrypted files
-openssl pkeyutl -encrypt -pubin -inkey publickey.crt -in $FileToEncrypt.FullName -out ($FileToEncrypt.FullName + ".encrypted")
+openssl pkeyutl -encrypt -pubin -inkey $PubKey -in $FileToEncrypt.FullName -out ($FileToEncrypt.FullName + ".encrypted")
 }
+
 # Deletes all unencrypted files in the folder
-Remove-Item -recurse C:\Users\administrator\Desktop\pass\* -exclude *.encrypted
+Remove-Item -recurse $FolderPath\* -exclude *.encrypted
